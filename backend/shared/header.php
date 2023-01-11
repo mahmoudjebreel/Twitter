082451d9f250cdc32b7c8d6ce0d6e72f65d37767
mjebreel <m.jebreel22@gmail.com>
2023-01-11 11:42:16 +0200

    first commit

---
+++ backend/shared/header.php
@@ -0,0 +1,17 @@
+<?php
+    if(!isset($pageTitle)){    
+        $pageTitle="Twitter.It's what happening / Twitter";
+    }
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?=$pageTitle?></title>
+    <link rel="shortcut icon" href="frontend/assets/favicon/twitter.ico" type="image/x-icon">
+    <link rel="stylesheet" href="frontend/assets/css/font-awesome/css/font-awesome.min.css">
+    <link rel="stylesheet" href="../Tweet/frontend/assets/css/master.css">
+</head>
+<body>
